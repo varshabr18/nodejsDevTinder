ccfa45b814ebbb0b76cefd4d199c3a351c564786
varshini R <121016194+varshabr18@users.noreply.github.com>
2026-05-07 11:34:20 +0530

    add midelware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("server responded for user request");
+app.get("/hello", (req, res, next) => {
+  const token = "xyz";
+  const isAuthorizedadmin = token === "xyz";
+  if (!isAuthorizedadmin) {
+    res.status(404).send("Admin not authorized");
+  } else {
+    next();
+  }
 });
+
+app.get("/hello/getAllData", (req, res) => {
+  res.send("got all data");
+});
+
+// app.get(/ab+cd/, (req, res) => {
+//   res.send({ firstname: "varsha", lastname: "gowda" });
+// });
+
+// app.get("/ab*cd", (req, res) => {
+//   res.send({ firstname: "varsha", lastname: "gowda" });
+// });
 
 app.listen("3000", () => {
   console.log("server up and running in 3000 posrt");
